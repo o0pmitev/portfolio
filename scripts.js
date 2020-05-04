f77b275b07f9b5729def678692a7ebc21c9f6692
Plamen Mitev <o0pmitev@linux.fritz.box>
2020-05-04 10:01:39 +0200

    initial commint

---
+++ scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  //smooth scrolling
+  var $root = $('html, body');
+  $('#navbarID a').click(function() {
+    var href = $.attr(this, 'href');
+    if (href != undefined && href != '#') {
+      $root.animate({
+        scrollTop: $(href).offset().top
+      }, 500, function() {
+        window.location.hash = href;
+      });
+    }
+    return false;
+  });
+});
+
+$(function() {
+  $('.nav-item a').on('click', function(){ 
+      if($('.navbar-toggler').css('display') !='none'){
+          $('.navbar-toggler').trigger( "click" );
+      }
+  });
+});
+

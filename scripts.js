c5c8c7707e0104e863c4d11ef768a8af9963c731
o0pmitev <pmitev89@gmail.com>
2020-05-05 15:46:40 +0200

    05-05-2020

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-$(document).ready(function(){
   //smooth scrolling
   var $root = $('html, body');
   $('#navbarID a').click(function() {
@@ -12,7 +11,7 @@ $(document).ready(function(){
     }
     return false;
   });
-});
+
 
 // hide navbar 
 $(function() {
@@ -26,3 +25,50 @@ $(function() {
 setInterval(function() {
   $('.blink').toggleClass('button-contact-me-color');
 }, 1000);
+
+
+// Projects
+const projects = document.querySelector('.projects-container');
+
+// projects
+const fetchUrl = './projects.json';
+fetch(fetchUrl)
+.then(response => response.json())
+.then(data => {
+
+// Create Projects
+  data.forEach(element => {
+    const projectTemplate =
+    `<div class="projects-wrapper">
+      <a href="#" class="image-link"target="_blank">
+        <div class="card">              
+          <img src="img/${element.image}" class="card-img" alt="...">              
+          <div class="card-img-overlay">
+            <h5 class="card-title">${element.projectTitle}</h5>
+            <p class="card-text hidden">${element.projectDescription}</p>
+          </div>
+        </div>
+      </a>
+    </div>`;
+    projects.innerHTML += projectTemplate;
+  });
+  
+  // Animate cards 
+  const getCard = document.querySelectorAll('.projects-wrapper');
+  const getText = document.querySelectorAll('.card-text');
+
+  // Hide Show project description
+  getCard.forEach((element,index) => {
+    element.addEventListener('mouseenter', function(){ 
+        getText[index].classList.add('show');   
+        getText[index].classList.remove('hidden');        
+     })
+    element.addEventListener('mouseleave', function() {
+      getText[index].classList.remove('show');        
+      getText[index].classList.add('hidden');        
+    })
+  })
+});
+
+
+
